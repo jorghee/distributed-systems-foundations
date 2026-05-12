0381a4c559c419d1de8cad1e56ed383b95a7ee35
Jorge Huarsaya <jorgeehuarsaya@gmail.com>
2026-05-12 09:40:04 -0500

    feat: Server Object Interface

---
+++ 4_rmi_java/medicines/MedicineInterface.java
@@ -0,0 +1,12 @@
+package medicines;
+
+import java.rmi.Remote;
+
+public interface MedicineInterface extends Remote {
+
+  public Medicine getMedicine(int amount) throws Exception;
+
+  public int getStock() throws Exception;
+
+  public String print() throws Exception;
+}
