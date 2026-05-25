83bc80c7abd6207df764742039baa3612471ce14
Jorge Huarsaya <jorgeehuarsaya@gmail.com>
2026-05-24 19:30:07 -0500

    build: agregar la inicialización del proyecto desde Spring Initializr

---
+++ assignments/distbank-concurrency/src/main/java/com/distbank/distbank_concurrency/DistbankConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.distbank.distbank_concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistbankConcurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistbankConcurrencyApplication.class, args);
+	}
+
+}
---
+++ assignments/distbank-concurrency/src/test/java/com/distbank/distbank_concurrency/DistbankConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distbank.distbank_concurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistbankConcurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
